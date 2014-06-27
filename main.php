6d5a5e0eb3a7e47a1690fa35bb5a77060cb1748f
Davit Barbakadze <jayarjo@gmail.com>
2014-06-27 18:01:01 +0400

    Localize i8 dependency.

--- main.php
+++ main.php
@@ -26,8 +26,10 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+require_once(dirname(__FILE__) . '/i8/class.Plugino.php');
 
-class WCE extends Plugino {
+
+class WCE extends WCE_Plugino {
 
 	function __construct()
 	{
